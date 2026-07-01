53ab0ff9ae1d5dfb5c66335f42756dbbf1eae96a
ch0fas <sof.mal.gar55@gmail.com>
2026-07-01 16:48:16 -0600

    yasss mas claro

---
+++ PreClase24.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class PreClase24
+{
+    public static void main(String[] args)
+    {
+        ArrayList<Integer> a1 = new ArrayList<>();
+        a1.add(10);
+        a1.add(20);
+        a1.add(30);
+        System.out.println("ArrayList Elements: ");
+        System.out.println(a1);
+
+        HashMap<Integer, String> m1 = new HashMap<>();
+        m1.put(8, "Zara");
+        m1.put(31, "Mahnaz");
+        m1.put(12, "Ayan");
+        m1.put(14, "Daisy");
+        System.out.println("HashMap Elements");
+        System.out.println(m1);
+    }
+}
